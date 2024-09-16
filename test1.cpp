54e51683dbb2654b508479cb06f552fc52fd5376
Ulyana Pazdzeyeva <ulyanapazdzeyeva@MacBook-Pro-Ulyana.local>
2024-09-16 22:41:13 +0300

    brain fuck

--- test1.cpp
+++ test1.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
-
+struct foo {
+    int value;
+};
+int operator+ (foo a, foo b) {
+    return 4;
+}
+// 2+2
+// 2 sum 2
+// +2 2
+// sum 2 2
+// sum (2,2)
+// 2+2*2
+// +*2 2 2
+// sum(mul(2,2),2)
+// *+2 2 2
+// mul(sum(2,2),2)
 int main() {
+    foo a, b;
+    std::cout << a + b << std::endl;
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
