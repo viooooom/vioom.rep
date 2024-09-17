7fe56bf9e1970054781a40124929cfe898d2c5df
vvelsone <velsonone@gmail.com>
2024-09-17 23:40:08 +0300

    commit_gleb

--- main.cpp
+++ main.cpp
@@ -4,5 +4,7 @@
 int main() {
   double result = perimeterOfSquare(5.0);
     std::cout << result << std::endl;
+    std::cout << "hi"<<std::endl;
+    std::cout << "bye"<<std::endl;
     return 0;
 }
